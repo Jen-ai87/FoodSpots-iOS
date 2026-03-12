58354e4ae55f7f066a3f487da28623b16996732f
Faraz Safdar <fsafdar2@gmail.com>
2026-03-12 14:39:35 -0400

    Add RestaurantCardView structure with distance text

--- FoodSpots/FoodSpots/Views/RestaurantCardView.swift
+++ FoodSpots/FoodSpots/Views/RestaurantCardView.swift
@@ -1,8 +1,27 @@
 import SwiftUI
 
-//TODO: Faraz 
 struct RestaurantCardView: View {
+    let restaurant: Restaurant
+    var onTap: (() -> Void)?
+    var onFavoriteTap: (() -> Void)?
+    var showDeleteButton: Bool = false
+    var onDeleteTap: (() -> Void)?
+
+    @EnvironmentObject var viewModel: RestaurantViewModel
+
+    var distanceText: String {
+        let d = restaurant.distance(from: viewModel.effectiveLocation)
+        return String(format: "%.1f mi", d)
+    }
+
     var body: some View {
-        
+        VStack(alignment: .leading, spacing: 0) {
+            Text(restaurant.name)
+                .font(.headline)
+            Text(distanceText)
+                .font(.caption)
+        }
+        .onTapGesture { onTap?() }
     }
 }
+
