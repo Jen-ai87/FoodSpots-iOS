2e853a175e4c8ea5ae527e60fbb99f0650275beb
Jen Henry <jokodzena@gmail.com>
2026-03-11 22:26:13 -0400

    Created Required Functions Distance, CusineTypes, CusineColor for Restaurant Model

--- FoodSpots/FoodSpots/Models/Restaurant.swift
+++ FoodSpots/FoodSpots/Models/Restaurant.swift
@@ -12,4 +12,38 @@ struct Restaurant: Identifiable, Codable, Hashable {
     var rating: Double
     var isFavorite: Bool
     var imageName: String
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    func distance(from location: CLLocation) -> Double {
+        let loc = CLLocation(latitude: latitude, longitude: longitude)
+        return location.distance(from: loc) / 1609.34
+    }
+    
+    static let cuisineTypes = [
+        "All", "Italian", "Japanese", "Mexican",
+        "American", "Chinese", "Indian", "Thai", "French", "Korean"
+    ]
+    
+    static let filterCuisineTypes = [
+        "Italian", "Japanese", "Mexican",
+        "American", "Chinese", "Indian", "Thai", "French", "Korean"
+    ]
+    
+    var cuisineColor: (start: String, end: String) {
+        switch cuisineType {
+        case "Italian":   return ("#E53935", "#EF9A9A")
+        case "Japanese":  return ("#5C6BC0", "#9FA8DA")
+        case "Mexican":   return ("#FB8C00", "#FFCC80")
+        case "American":  return ("#F9A825", "#FFF176")
+        case "Chinese":   return ("#C62828", "#EF9A9A")
+        case "Indian":    return ("#EF6C00", "#FFCC80")
+        case "Thai":      return ("#2E7D32", "#A5D6A7")
+        case "French":    return ("#6A1B9A", "#CE93D8")
+        case "Korean":    return ("#AD1457", "#F48FB1")
+        default:          return ("#FF6D00", "#FFD180")
+        }
+    }
 }
