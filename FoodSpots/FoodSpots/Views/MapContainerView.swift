5a2ee783b55bc9d734a33486454e6eb3cd4e8778
Faraz Safdar <fsafdar2@gmail.com>
2026-03-12 15:13:25 -0400

    Add basic map container with camera state and sheet

--- FoodSpots/FoodSpots/Views/MapContainerView.swift
+++ FoodSpots/FoodSpots/Views/MapContainerView.swift
@@ -1,8 +1,35 @@
 import SwiftUI
+import MapKit
 
-//TODO: Faraz 
 struct MapContainerView: View {
+    @EnvironmentObject var viewModel: RestaurantViewModel
+    @State private var mapCenter = CLLocationCoordinate2D(latitude: 43.6532, longitude: -79.3832)
+    @State private var zoomMeters: Double = 4000
+    @State private var position: MapCameraPosition = .region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 43.6532, longitude: -79.3832),
+        latitudinalMeters: 4000,
+        longitudinalMeters: 4000
+    ))
+    @State private var selectedRestaurant: Restaurant?
+    @State private var showDrawer = false
+    @State private var isFollowingUser = false
+
     var body: some View {
-        
+        ZStack(alignment: .topLeading) {
+            mapView
+        }
+        .sheet(item: $selectedRestaurant) { r in
+            RestaurantDetailDrawer(restaurant: r)
+                .presentationDetents([.fraction(0.45)])
+                .presentationDragIndicator(.hidden)
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+
+    @ViewBuilder
+    private var mapView: some View {
+        Map(position: $position) { }
+            .mapStyle(.standard)
+            .ignoresSafeArea()
     }
 }
